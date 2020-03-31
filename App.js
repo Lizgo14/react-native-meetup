af71f5371330fd28214db9bd902c839dc5a8dc08
Lizgo14 <lizgordon14@gmail.com>
2020-03-31 19:14:24 -0400

    misc: initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+//<View> is the same as <div>
+//<Text> is the same as  <p>, <h1>....
