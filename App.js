ad5120b4c26a13621aac3c447139787e47b5c41e
Lizgo14 <lizgordon14@gmail.com>
2020-03-31 19:37:36 -0400

    First commit

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//adding an image
+import SampleIcon from './assets/icon.png'
+
 export default function App() {
   return (
 
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+    <Image style={styles.stretch} source= {SampleIcon}/>
     </View>
   );
 }
+
+//can use this info to scale to device in style
+const screenHeight = Dimensions.get('window').height
+const screenWidth = Dimensions.get('window').width
 
 //usually camelcase instead of dashes
 const styles = StyleSheet.create({
@@ -18,6 +26,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  stretch:{
+    width: screenWidth/2,
+    height:400,
+    resizeMode: 'stretch'
+  }
 });
 
 
